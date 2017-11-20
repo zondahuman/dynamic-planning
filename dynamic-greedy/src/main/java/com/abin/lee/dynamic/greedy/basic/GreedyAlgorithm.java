cf472dcc31b87c2a1b0a4a661d29cc237f2de235
zondahuman <zondahuman@gmail.com>
2017-11-21 00:29:58 +0800

    add greedy

---
+++ dynamic-greedy/src/main/java/com/abin/lee/dynamic/greedy/basic/GreedyAlgorithm.java
@@ -0,0 +1,14 @@
+package com.abin.lee.dynamic.greedy.basic;
+
+/**
+ * Created by abin on 2017/11/21 2017/11/21.
+ * dynamic-planning
+ * com.abin.lee.dynamic.greedy.basic
+ * BestLoading
+ */
+public class GreedyAlgorithm {
+    public static void main(String[] args) {
+        
+    }
+
+}
