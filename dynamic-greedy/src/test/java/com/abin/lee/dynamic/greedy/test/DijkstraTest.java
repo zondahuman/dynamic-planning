1386bfcbfad9e9415a94c634fba545916d00d6d7
zondahuman <zondahuman@gmail.com>
2017-11-25 22:15:20 +0800

    add

---
+++ dynamic-greedy/src/test/java/com/abin/lee/dynamic/greedy/test/DijkstraTest.java
@@ -0,0 +1,56 @@
+package com.abin.lee.dynamic.greedy.test;
+
+/**
+ * Created by abin on 2017/11/25 2017/11/25.
+ * dynamic-planning
+ * com.abin.lee.dynamic.plan.graph.dijkstra.test
+ */
+public class DijkstraTest {
+    public static int MAX = 10000;
+
+    public static void main(String[] args) {
+        //邻接矩阵
+        int[][] weight = {
+                {0, 3, 2000, 7, MAX},
+                {3, 0, 4, 2, MAX},
+                {MAX, 4, 0, 5, 4},
+                {7, 2, 5, 0, 6},
+                {MAX, MAX, 4, 6, 0}
+        };
+        int start = 0;
+        int[] shortPath = dijkstra(weight, start);
+        for (int i = 0; i < shortPath.length; i++) {
+            System.out.println("从" + start + "出发到" + i + "的最短距离为：" + shortPath[i]);
+        }
+
+    }
+
+    public static int[] dijkstra(int[][] weight, int start){
+        int length = weight.length;
+        int[] shortPath = new int[length];
+        int[] visited = new int[length];
+        visited[start] = 1;
+        shortPath[start] = 0;
+        for (int count = 1; count < length; count++) {
+            int k = -1;
+            int dest = Integer.MAX_VALUE;
+            for(int i=0; i<length;i++){
+                if(visited[i] == 0 && weight[start][i] < dest){
+                    dest = weight[start][i];
+                    k = i;
+                }
+            }
+            shortPath[k] = dest;
+            visited[k] = 1;
+            for(int i=0; i<length;i++){
+                if(visited[i] == 0 && weight[start][k]+weight[k][i] < weight[start][i]){
+                    weight[start][i] = weight[start][k] + weight[k][i];
+                }
+            }
+        }
+        return shortPath;
+    }
+
+
+
+}
---
+++ dynamic-greedy/src/test/java/com/abin/lee/dynamic/greedy/test/GreedyAlgorithmTest.java
@@ -0,0 +1,56 @@
+package com.abin.lee.dynamic.greedy.test;
+
+/**
+ * Created by abin on 2017/11/25 2017/11/25.
+ * dynamic-planning
+ * com.abin.lee.dynamic.plan.graph.pack.test
+ */
+public class GreedyAlgorithmTest {
+
+    public static void main(String[] args) {
+        int m = 10;
+        int n = 3;
+        int w[] = {3, 4, 5};
+        int p[] = {4, 5, 6};
+        int c[][] = pack(m, n, w, p);
+//        System.out.printf("c[][] = "+ JsonUtil.toJson(c));
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                System.out.print(c[i][j] + "\t");
+                if (j == m-1) {
+                    System.out.println();
+                }
+            }
+        }
+    }
+
+    /**
+     *
+     * @param m  表示背包的重量
+     * @param n  表示物件的个数
+     * @param w  物件的重量数组
+     * @param p  物价的价值数组
+     * @return
+     */
+    public static int[][] pack(int m, int n, int[] w, int[] p){
+        //前n个物件放入了承重为m的背包所能得到的最大价值
+        int[][] maxValue = new int[n][m];
+        for (int i = 1; i <n ; i++)
+            maxValue[i][0] = 0;
+        for (int i = 1; i <m ; i++)
+            maxValue[0][i] = 0;
+        for (int i = 1; i <n ; i++) {
+            for (int j = 1; j <m ; j++) {
+                if(w[i] < j){
+                        maxValue[i][j] = Math.max(maxValue[i-1][j-w[i]]+p[i], maxValue[i-1][j]);
+                }else{
+                    maxValue[i][j] = maxValue[i-1][j];
+                }
+            }
+        }
+        return maxValue;
+    }
+
+
+
+}
