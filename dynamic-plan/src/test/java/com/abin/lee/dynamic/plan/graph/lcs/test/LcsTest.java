c781723942f8d55b9f2bc98cd0a7c9d3e1c41523
zondahuman <zondahuman@gmail.com>
2017-12-02 01:14:37 +0800

    add

--- dynamic-plan/src/test/java/com/abin/lee/dynamic/plan/graph/lcs/test/LcsTest.java
+++ dynamic-plan/src/test/java/com/abin/lee/dynamic/plan/graph/lcs/test/LcsTest.java
@@ -11,20 +11,15 @@ public class LcsTest {
 
     public static int lcs(String param1, String param2) {
         int[][] dp = new int[param1.length() + 1][param2.length() + 1];
-        for(int row = 0; row <= param1.length(); row++)
-            dp[row][0] = 0;
-        for(int column = 0; column <= param2.length(); column++)
-            dp[0][column] = 0;
-        for (int i = 1; i <= param1.length(); i++) {
-            for (int j = 1; j <= param2.length(); j++) {
-                    if (param1.charAt(i - 1) == param2.charAt(i - 1))
-                        dp[i][j] = dp[i - 1][j - 1] + 1;
-                    else
-                        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+        for (int i = 0; i <= param1.length(); i++)
+            for (int j = 0; j <= param2.length(); j++) {
+                if (i == 0 || j == 0)
+                    dp[i][j] = 0;
+                else if (param1.charAt(i - 1) == param2.charAt(j - 1))
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                else
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
             }
-        }
-//        int val =  dp[param1.length()][param2.length()];
-//        return param1.length() - val + param2.length() - val;
         return dp[param1.length()][param2.length()];
     }
 
