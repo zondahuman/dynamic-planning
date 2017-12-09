9222826677e0701ed161c9010c50e65c8ed36b1e
zondahuman <zondahuman@gmail.com>
2017-12-09 22:48:15 +0800

    add

--- dynamic-greedy/src/test/java/com/abin/lee/dynamic/greedy/basic/gas/test/CarStationRefuelTest.java
+++ dynamic-greedy/src/test/java/com/abin/lee/dynamic/greedy/basic/gas/test/CarStationRefuelTest.java
@@ -10,7 +10,26 @@ public class CarStationRefuelTest {
         int[] distance = new int[]{1,2,3,4,5,6,6};
         int totalJourney = 7;
         int result = carRefuel(distance, totalJourney);
+//        int result = addRefuel(distance, totalJourney);
         System.out.println("result=" + result);
+    }
+
+
+    public static int addRefuel(int[] distance, int journey){
+        for (int i = 0; i < distance.length; i++) {
+            if(distance[i] > journey)
+                return -1;
+        }
+        int sum = 0;
+        int num = 0;
+        for (int i = 0; i < distance.length; i++) {
+            sum+=distance[i];
+            if(sum > journey){
+                num++;
+                sum = distance[i];
+            }
+        }
+        return num;
     }
 
     public static int carRefuel(int[] distance, int totalJourney){
